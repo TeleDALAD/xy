720a6ab38e48668d4b4902d1195d946d4ea28c08
TeleDALAD <ashkankorouei@gmail.com>
2016-03-04 08:28:24 +0330

    Update leave.lua

--- plugins/leave.lua
+++ plugins/leave.lua
@@ -1,21 +1,22 @@
 do
-function run(msg, matches)
-  local user = 'user#id'..our_id
-  local chat = 'chat#id'..msg.to.id
-  chat_del_user(chat, user, ok_cb, false)
+-- Will leave the group if be added
+local function run(msg, matches)
+local bot_id = our_id -- your bot id
+   -- like local bot_id = 1234567
+    if matches[1] == 'leave' and is_admin(msg) then
+       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
+    elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
+      send_large_msg("chat#id"..msg.to.id, 'this ', ok_cb, false)
+      chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
+      block_user("user#id"..msg.from.id,ok_cb,false)
+    end
 end
-
-
+ 
 return {
-    description = "leaves the group ",
-    usage = "!leave : با دستور سازنده از گروه لفت میدهد",
-    patterns = {
-	  "^/leave$",
-	  "^!leave$",
-	  "^leave$"
-    },
-    run = run,
-    privileged = true
+  patterns = {
+    "^[!/](leave)$",
+    "^!!tgservice (.+)$",
+  },
+  run = run
 }
-
 end
