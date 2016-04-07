99d4825e8ab0352d57bcb10006f5a14978731d47
TeleDALAD <ashkankorouei@gmail.com>
2016-04-07 12:30:28 +0430

    Update x.lua

--- plugins/x.lua
+++ plugins/x.lua
@@ -1,31 +1,19 @@
 do
 local function run(msg, matches)
-local bot_id = 141136595
-local x = 175623013
-local y = 152457798
-local z = 169975536
-local w = 203355385
+local bot_id = id-bot141136595
 
-    if matches[1] == 'bye' and is_admin(msg) or msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
+
+    if matches[1] == 'left' and is_admin(msg) or msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
-    elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(x) then
-       chat_add_user("chat#id"..msg.to.id, 'user#id'..x, ok_cb, false)
-    end
-     elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(y) then
-       chat_add_user("chat#id"..msg.to.id, 'user#id'..y, ok_cb, false)
-    end
-     elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(z) then
-       chat_add_user("chat#id"..msg.to.id, 'user#id'..z, ok_cb, false)
-    end
-     elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(w) then
-       chat_add_user("chat#id"..msg.to.id, 'user#id'..w, ok_cb, false)
+    elseif msg.action.type == "chat_del_user" and is_admin(msg.action.user.id) then
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..msg.action.user.id, ok_cb, false)
     end
 end
  
 return {
   patterns = {
-    "^[!/](bye)$",
-    "^!!tgservice (.+)$",
+    "^[!/]([Ll][Ee][Ff][Tt])$",
+    "^!!tgservice (.+)$"
   },
   run = run
 }
